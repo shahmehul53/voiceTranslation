71dc8ee9465f8f823e2b9197a73ff2e1d3db17ce
shahmehul53 <mehul.developer2019@gmail.com>
2022-04-10 14:36:59 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View} from 'react-native';
+//Screens
+import {Route} from './src/Route';
+
+export default App = () => {
+  return (
+    <View flex={1}>
+      <Route />
+    </View>
+  );
+};
